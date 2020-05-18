838e58a93597072b8996032ba73c5a0f1edbac26
上野誉司 <isacatoneu@uenohomareshinoMacBook-puro.local>
2020-05-18 14:16:47 +0900

    Initial Commit Message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
   def show
   	@book = Book.find(params[:id])
     @user = @book.user
+    @post_comment = PostComment.new
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   root  to: 'home#top'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-
+  resources :books do
+  	# いいね機能とコメント機能
+  	resource :favorites, only: [:create, :destroy]
+ 	resource :post_comments, only: [:create]
+  end
 
   get 'home/about' ,to: "home#about"
  end
